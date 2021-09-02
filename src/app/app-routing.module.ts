6776cc4116ded441fdbd995c0d7c7fd570a502be
CrisAmauri <cristian.chavero@agilethought.com>
2021-09-02 18:24:27 -0500

    Se incorpora segunda pantalla que es el curriculum donde se proyecta la información del perfil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
+import { CurriculumComponent } from './components/curriculum/curriculum.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {path:'', component:CardComponent},
+  {path:'curriculum', component:CurriculumComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from '@angular/router';
 // Libreria AgileThought
 import { ATComponentsModule } from 'at-digital-components';
+// Mis componentes
 import { CardComponent } from './components/card/card.component';
+import { CurriculumComponent } from './components/curriculum/curriculum.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CurriculumComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ATComponentsModule
+    ATComponentsModule,
+    RouterModule,
+    HttpClientModule
 
   ],
   providers: [],
